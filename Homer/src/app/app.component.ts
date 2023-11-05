6c82fdc1ba12b7dc8fb16f98a0fff0e9820f10c4
jbthelegend <jbthelegend15@gmail.com>
2023-11-05 04:40:18 -0600

    listing

---
+++ Homer/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cl-root',
+  templateUrl: './app.component.html',
+  /*selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']*///from chatgpt
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+  title = 'Homer';
+  ngOnInit(): void {}
+}
