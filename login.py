b8cc82e9e89a86af2a98aa2ab0c497b49ecec94a
jbthelegend <145824106+jbthelegend@users.noreply.github.com>
2023-11-05 08:38:08 -0600

    Create login.py

---
+++ login.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import csv
+
+def main():
+    st.title("Login Page")
+    # Create a dropdown for selecting a role
+    
+    valid = False
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"): 
+        st.session_state.page = 'insight'
+
+def getRole():
+    if 'selected_role' in st.session_state:
+        return st.session_state.selected_role
+    else:
+        return None
