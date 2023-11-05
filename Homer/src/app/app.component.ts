f0dc3215619660a8bad1da6671490aa466fead5c
jbthelegend <jbthelegend15@gmail.com>
2023-11-04 21:52:49 -0500

    first page done

---
+++ Homer/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cl-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
---
+++ Homer/src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import type { OnInit } from "@angular/core";
+
+/* @figmaId 7:37 */
+@Component({
+  selector: "cl-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
