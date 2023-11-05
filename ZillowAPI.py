47cead0986a73919ca0184cc6f0d229ef5756aab
NoelEmmanuel <79769249+NoelEmmanuel@users.noreply.github.com>
2023-11-05 01:59:38 -0600

    ZillowChanges

--- ZillowAPI.py
+++ ZillowAPI.py
@@ -1,49 +1,29 @@
-
 import requests
 import json
 
-#/ Set your API key and ZPID of a property
+#/ Set your API key 
 
-api_key = "93a177e2-5c9b-439d-b4c6-1681240fbd32"  # CHANGE WITH YOUR API KEY
-property_zpid = "2054866553"
-query = "3452 Canyon Lake Dr, Little Elm, TX"
-
-
-querystring = {
-  "api_key": api_key,
-  "street":"",
-  "city":"Little Elm",
-  "state":"TX",
-  "zipcode":"75068"
-}
-
-#/ API endpoint and default headers
-api_url = "https://app.scrapeak.com/v1/scrapers/zillow"
-
-
-paramP = {"api_key": api_key, "zpid": property_zpid}
-
+api_key = "93a177e2-5c9b-439d-b4c6-1681240fbd32"
 
 #/ Make the API request
-
-def get_property_details_by_address(api_key, address, city, state, zipcode):
-    api_url = "https://app.scrapeak.com/v1/scrapers/zillow"
+def get_property_details_by_address(city, state, zipcode):
+    api_url = "https://app.scrapeak.com/v1/scrapers/zillow/zpidByAddress"
     querystring = {
         "api_key": api_key,
-        "street": address,
+        "street": "",
         "city": city,
         "state": state,
         "zipcode": zipcode
     }
-    response = requests.get(api_url + "/zpidByAddress", params=querystring)
+    response = requests.get(api_url, params=querystring)
     return response.text
 
 def get_property_details_by_zpid(api_key, property_zpid):
-    api_url = "https://app.scrapeak.com/v1/scrapers/zillow"
+    api_url = "https://app.scrapeak.com/v1/scrapers/zillow/property"
     paramP = {"api_key": api_key, "zpid": property_zpid}
-    response = requests.get(api_url + "/property", params=paramP)
+    response = requests.get(api_url, params=property_zpid)
     return response.text
 
 
-
+# print(get_property_details_by_zpid(api_key, 26914709))
 
